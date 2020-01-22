43bac8fccf928ad2da97d00c300bad6b94e0ebb5
DarkMarJak <58727537+DarkMarJak@users.noreply.github.com>
2020-01-22 18:00:52 +0000

    Upload VS_React

---
+++ VS_NodeJs/VS_React/ClientApp/src/components/Produtos.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Dashboard extends Component {
+    render() {
+      return (
+        <div className="Component">
+            xd
+        </div>
+      );
+    }
+  }
+  
+  export default Dashboard;

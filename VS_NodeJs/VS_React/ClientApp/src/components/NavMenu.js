aee1167d5f574e805c4804d46a78ea9db12f7e58
Milkenm <miguel.campos.johnny@gmail.com>
2020-02-03 10:19:06 +0000

    Olha, já funciona

--- VS_NodeJs/VS_React/ClientApp/src/components/NavMenu.js
+++ VS_NodeJs/VS_React/ClientApp/src/components/NavMenu.js
@@ -5,42 +5,50 @@ import { LinkContainer } from 'react-router-bootstrap';
 import '../css/NavMenu.css';
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
+	displayName = NavMenu.name
 
-  render() {
-    return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to={'/'}>Bar Escolar</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='apple' /> Produtos
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/carrinho'}>
-              <NavItem>
-                <Glyphicon glyph='shopping-cart' /> Carrinho
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/login'}>
-              <NavItem>
-                <Glyphicon glyph='log-in' /> Login
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/dashboard'}>
-              <NavItem>
-                <Glyphicon glyph='list-alt' /> Dashboard
-              </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+	render() {
+		var dashboard;
+		if (true) {
+			dashboard = (
+				<LinkContainer to={'/dashboard'}>
+				<NavItem>
+					<Glyphicon glyph='list-alt' /> Dashboard
+								</NavItem>
+			</LinkContainer>);
+		}
+
+		return (
+			<Navbar inverse fixedTop fluid collapseOnSelect>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<Link to={'/'}>Bar Escolar</Link>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav>
+						<LinkContainer to={'/'} exact>
+							<NavItem>
+								<Glyphicon glyph='apple' /> Produtos
+							 </NavItem>
+						</LinkContainer>
+
+						<LinkContainer to={'/carrinho'}>
+							<NavItem>
+								<Glyphicon glyph='shopping-cart' /> Carrinho
+							  </NavItem>
+						</LinkContainer>
+
+						<LinkContainer to={'/login'}>
+							<NavItem>
+								<Glyphicon glyph='log-in' /> Login
+							</NavItem>
+						</LinkContainer>
+						{dashboard}
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
 }
